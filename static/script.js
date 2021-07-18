52e439a194bdb7210ef4d7ca8d246139b5995e52
Sourabh Choure <36518515+saviourcode@users.noreply.github.com>
2021-07-18 02:50:10 -0500

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,6 +1,22 @@
 $(function () {
-    $("#test").click(function () {
+    $("#kmeans").click(function () {
 	$.getJSON('/kmeans/',
+                function(data) {
+              $("#me").css("visibility", "visible");
+        	$("#me").html(data);
+        });
+        
+    })
+    $("#iso").click(function () {
+	$.getJSON('/isolation/',
+                function(data) {
+              $("#me").css("visibility", "visible");
+        	$("#me").html(data);
+        });
+        
+    })
+    $("#hbos").click(function () {
+	$.getJSON('/hbos/',
                 function(data) {
               $("#me").css("visibility", "visible");
         	$("#me").html(data);
